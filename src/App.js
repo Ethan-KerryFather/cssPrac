05015c22683e2f714c70b712162becc27450f720
Rimoosoo <studentlimsoo@gmail.com>
2023-07-03 18:21:39 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import "./styles.scss";
+
+export default function App() {
+  return (
+    <div className="App">
+      <p>Hello CodeSandbox</p>
+      <h1
+        className="glitch"
+        data-text="Start editing to see some magic happen!"
+      >
+        Start editing to see some magic happen!
+      </h1>
+
+      <div className="goup">
+        <div className="goup2">
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            users
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            all
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            SHUSHUK
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            MINES
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            YOURS
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            QVE
+          </span>
+          <span style={{ fontSize: "2em", display: "block", padding: "0px" }}>
+            INVINCIBLE NODE
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+}
